d2f13b25989e2dd0b42c3d13317c3d2625e26f35
Shivam Kaushik <shivamkaushik14495@gmail.com>
2024-03-14 12:58:53 +0530

    updated image

--- src/components/body.js
+++ src/components/body.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import testing from './sample-image.jpg'
-import './body.css'
+// import '.data.js';
+import testing from "./Receipt-Generator.jpg"
+import './body.css';
 const Body = () => {
   return (
     <div className='tools'>
-        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
-        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
-        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
-        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
-        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
         <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
     </div>
   )
---
+++ src/components/data.js
@@ -0,0 +1,8 @@
+const data = [
+    {
+        "id" : 1,
+        "tooName":'Invoice Generator',
+        "imglink":"./Receipt-Generator.jpg",
+        "toollink":""
+    }
+]
