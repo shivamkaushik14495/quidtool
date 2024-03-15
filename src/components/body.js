d1980dc841a5d4164041804aa8519f7e3b1afa12
Shivam Kaushik <shivamkaushik14495@gmail.com>
2024-03-15 12:24:16 +0530

    responsve V3

--- src/components/body.js
+++ src/components/body.js
@@ -8,6 +8,15 @@ const Body = () => {
       {/* Uncomment this to fetch data from json file */}
       {/* {data.tooldata.map(user =>(<a className='tool' href='./Quid_tool.png' key={user.id}><img src={user.imglink} alt={user.tooName}></img></a>))} */}
         <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
+        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
+        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
+        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
+        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
+        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
+        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
+        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
+        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
+        <a className='tool' href='./Quid_tool.png'><img src={testing} alt='toolimage'></img></a>
     </div>
   )
 }
